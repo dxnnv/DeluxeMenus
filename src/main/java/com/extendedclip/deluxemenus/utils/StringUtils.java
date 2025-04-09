c73c9a0f544d6d27bfa80de563c83f46723cbfe7
danny <danny@atropos.cc>
2025-04-08 21:48:26 -0400

    feat: add serialization, fix deserialization
    Signed-off-by: danny <danny@atropos.cc>

--- src/main/java/com/extendedclip/deluxemenus/utils/StringUtils.java
+++ src/main/java/com/extendedclip/deluxemenus/utils/StringUtils.java
@@ -2,6 +2,8 @@ package com.extendedclip.deluxemenus.utils;
 
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.command.CommandSender;
@@ -11,26 +13,52 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 
+import static net.kyori.adventure.text.minimessage.MiniMessage.miniMessage;
+
 public class StringUtils {
 
     private static final GsonComponentSerializer gson = GsonComponentSerializer.gson();
-    private static final LegacyComponentSerializer serializer = LegacyComponentSerializer.builder().hexColors().build();
+    private static final LegacyComponentSerializer legacySerializer = LegacyComponentSerializer.legacyAmpersand().toBuilder().hexColors().build();
 
     /**
-     * Transforms a string into a {@link Component} with support for legacy color codes and hex color codes (1.16.1+).
+     * Converts the provided input string containing MiniMessage formatted text into a deserialized {@link Component}.
      *
-     * @param input The input string containing legacy or hex color codes.
-     * @return A {@link Component} representing the input string with color codes parsed.
+     * @param input The input string containing MiniMessage formatted text. Must not be null.
+     * @return The deserialized {@link Component} representation of the input string.
      */
     @NotNull
     public static Component color(@NotNull String input) {
-        return serializer.deserialize(input);
+        return miniMessage().deserialize(input).decorationIfAbsent(TextDecoration.ITALIC, TextDecoration.State.FALSE);
     }
 
+    /**
+     * Serializes a string containing text into the MiniMessage format,
+     * then deserializes the component back into a MiniMessage string.
+     *
+     * @param input The text input to be serialized into MiniMessage. Must not be null.
+     * @return A MiniMessage-formatted string representation of the input text.
+     */
+    public static String serializeMiniMessage(String input) {
+        TextComponent component = legacySerializer.deserialize(input);
+        return miniMessage().serialize(component);
+    }
+
+    /**
+     * Sends a JSON-formatted message to the given {@link CommandSender}.
+     *
+     * @param sender The {@link CommandSender} to whom the JSON-formatted message should be sent.
+     * @param json The JSON-formatted string message to be sent.
+     */
     public static void sendJson(CommandSender sender, String json) {
         sender.sendMessage(fromJson(json));
     }
 
+    /**
+     * Deserializes the given JSON-formatted string into a {@link Component} object.
+     *
+     * @param json The JSON-formatted string to deserialize. Must not be null.
+     * @return The deserialized {@link Component} object represented by the input JSON string.
+     */
     public static Component fromJson(String json) {
         return gson.deserialize(json);
     }
